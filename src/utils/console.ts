d7843bd6f4e2be49ead51327598df51d137c17f5
Agustrihanton <agustrihanton97@gmail.com>
2023-05-24 13:32:43 +0700

    feat: add environment add console

--- src/utils/console.ts
+++ src/utils/console.ts
@@ -1,4 +1,4 @@
-import { NODE_ENV } from "config/environment";
+import { NODE_ENV } from "src/config/environment";
 
 export default function consoleDev(message: any) {
   if (NODE_ENV === 'development') {
